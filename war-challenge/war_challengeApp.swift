708b49ad8285c01ab093d847f488279a1c756f49
julian erlandson <julianerlandson@gmail.com>
2022-07-09 16:12:58 +0200

    Initial Commit

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Julian Erlandsson on 2022-07-09.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
